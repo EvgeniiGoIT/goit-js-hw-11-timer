77d15dc2c0d002128c2080af47f957776b241cad
Evgenii <wojda007@gmail.com>
2019-06-23 21:20:22 +0300

    finish

---
+++ index.js
@@ -0,0 +1,69 @@
+class CountdownTimer {
+  constructor({ selector, targetDate }) {
+    this.selector = document.querySelector(selector);
+    this.targetDate = targetDate;
+    this.refs = {
+      days: this.selector.querySelector('[data-value="days"]'),
+      hours: this.selector.querySelector('[data-value="hours"]'),
+      mins: this.selector.querySelector('[data-value="mins"]'),
+      secs: this.selector.querySelector('[data-value="secs"]')
+    };
+  }
+
+  start() {
+    this.timer = setInterval(() => {
+      if (this.targetDate <= Date.now()) {
+        this.stop();
+        return;
+      }
+      const time = this.targetDate - Date.now();
+      /*
+       * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
+       * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
+       */
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+      /*
+       * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
+       * остатка % и делим его на количество миллисекунд в одном часе
+       * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
+       */
+      const hours = Math.floor(
+        (time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+
+      /*
+       * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
+       * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
+       */
+      const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+
+      /*
+       * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
+       * миллисекунд в одной секунде (1000)
+       */
+      const secs = Math.floor((time % (1000 * 60)) / 1000);
+      this.updateClockface(days, hours, mins, secs);
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.timer);
+  }
+
+  updateClockface(...args) {
+    args.map((e, i) => {
+      Object.values(this.refs)[i].textContent = this.pad(e);
+    });
+  }
+  pad(value) {
+    return String(value).padStart(2, "0");
+  }
+}
+
+const timer = new CountdownTimer({
+  selector: "#timer-1",
+  targetDate: new Date("June 24, 2019")
+});
+
+timer.start();
